54b5a34e35c1f1675c9cfad159177939a9dadbed
Austin Perrine <akperrine@gmail.com>
2022-09-05 19:21:25 -0600

    style nav bar with logo and log out button

--- src/components/WeeklyPlan/weeklyPlan.jsx
+++ src/components/WeeklyPlan/weeklyPlan.jsx
@@ -1,27 +1,23 @@
 import "./weeklyPlan.css";
+import logoWord from "../../assets/logo-word.png";
 import DailyPlan from "./DailyPlan/dailyPlan";
 
 const WeeklyPlan = ({ username, weeksWorkouts }) => {
   return (
-    <div>
-      <h2>Welcome {username}</h2>
-      {weeksWorkouts.map((workoutDay) => {
-        const arrayLength = workoutDay.workout.length;
-        return <DailyPlan arrayLength={arrayLength} workoutDay={workoutDay} />;
-        {
-          /* return (
-          <div>
-            <h6>Workout for day {workoutDay.day}</h6>
-            {workoutArrLength > 0 ? (
-              workoutDay.workout.map((workout) => <p>{workout}</p>)
-            ) : (
-              <p>There is nothing programmed for today</p>
-            )}
-            {workoutArrLength > 0 ? <button>Workout Completed</button> : null}
-          </div>
-        ); */
-        }
-      })}
+    <div className="client-container">
+      <div className="workout-week">
+        <nav className="nav">
+          <img src={logoWord} className="nav-logo" />
+          <button className="logout-btn btn">Log Out</button>
+        </nav>
+        <h2 className="welcome-header">Welcome {username}</h2>
+        {weeksWorkouts.map((workoutDay) => {
+          const arrayLength = workoutDay.workout.length;
+          return (
+            <DailyPlan arrayLength={arrayLength} workoutDay={workoutDay} />
+          );
+        })}
+      </div>
     </div>
   );
 };
