0bced1419efc37aa10a95c6a469183c006c01863
Austin Perrine <akperrine@gmail.com>
2023-01-30 15:01:07 -0700

    add useState to open and close nav dropdown

--- src/routes/navigation/navigation.jsx
+++ src/routes/navigation/navigation.jsx
@@ -3,35 +3,42 @@ import { useDispatch } from "react-redux";
 import logoWord from "../../assets/logo-word.png";
 import { logout } from "../../redux/features/user/userSlice";
 import { NavLink, Outlet } from "react-router-dom";
-import { Fragment } from "react";
+import React, { Fragment } from "react";
 import { IoIosArrowDown } from "react-icons/io";
 import "./navigation.css";
 
 const Navigation = () => {
+  const [dropdown, setDropdown] = React.useState(false);
   const dispatch = useDispatch();
+
+  const handleDropdownClick = (event) => {
+    event.preventDefault();
+    setDropdown(!dropdown);
+  };
 
   const handleSignout = (event) => {
     event.preventDefault();
     dispatch(logout());
   };
   return (
-    <Fragment>
+    <>
       <nav className="nav">
         <NavLink to="/">
           <img src={logoWord} className="nav-logo" alt="Perrine Athletics" />
         </NavLink>
         <div className="nav-button-box">
-          <button className="dropdown-btn btn">
+          <button className="dropdown-btn btn" onClick={handleDropdownClick}>
             <IoIosArrowDown />
           </button>
-          <NavDropdown />
+          {dropdown && <NavDropdown />}
+
           <button className="logout-btn btn" onClick={handleSignout}>
             Log Out
           </button>
         </div>
       </nav>
       <Outlet />
-    </Fragment>
+    </>
   );
 };
 
